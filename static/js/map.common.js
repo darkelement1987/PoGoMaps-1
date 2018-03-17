0f47ff9a4629b5cc28fe4a3513d3e9dadd88c74c
darkelement1987 <jgernaat1987@gmail.com>
2018-03-18 00:19:13 +0100

    Update map.common.js

--- static/js/map.common.js
+++ static/js/map.common.js
@@ -859,7 +859,7 @@ var StoreOptions = {
         type: StoreTypes.JSON
     },
     'remember_select_rarity_notify': {
-        default: [], // Common, Uncommon, Rare, Very Rare, Ultra Rare
+        default: [], // Common, Uncommon, Rare, Very Rare, Ultra Rare, New Spawn
         type: StoreTypes.JSON
     },
     'remember_text_perfection_notify': {
@@ -871,7 +871,7 @@ var StoreOptions = {
         type: StoreTypes.Number
     },
 	'excludedRarity': {
-        default: 0, // 0: none, 1: <=Common, 2: <=Uncommon, 3: <=Rare, 4: <=Very Rare, 5: <=Ultra Rare
+        default: 0, // 0: none, 1: <=Common, 2: <=Uncommon, 3: <=Rare, 4: <=Very Rare, 5: <=Ultra Rare, 6: <=New Spawn
         type: StoreTypes.Number
     },
     'showRaids': {
@@ -1007,7 +1007,7 @@ var StoreOptions = {
         type: StoreTypes.String
     },
     'iconSizeModifier': {
-        default: 10,
+        default: 0,
         type: StoreTypes.Number
     },
     'scaleByRarity': {
@@ -1200,6 +1200,7 @@ function setupPokemonMarkerDetails(item, map, scaleByRarity = true, isNotifyPkmn
 
     if (scaleByRarity) {
         const rarityValues = {
+            'new spawn': 40,
             'very rare': 30,
             'ultra rare': 40,
             'legendary': 50
