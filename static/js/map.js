6d736250af319f7649cab2dc64c7b2e009aedfef
darkelement1987 <jgernaat1987@gmail.com>
2018-03-18 00:19:40 +0100

    Update map.js

--- static/js/map.js
+++ static/js/map.js
@@ -114,7 +114,8 @@ const excludedRaritiesList = [
   [i8ln('Common'), i8ln('Uncommon')],
   [i8ln('Common'), i8ln('Uncommon'), i8ln('Rare')],
   [i8ln('Common'), i8ln('Uncommon'), i8ln('Rare'), i8ln('Very Rare')],
-  [i8ln('Common'), i8ln('Uncommon'), i8ln('Rare'), i8ln('Very Rare'), i8ln('Ultra Rare')]
+  [i8ln('Common'), i8ln('Uncommon'), i8ln('Rare'), i8ln('Very Rare'), i8ln('Ultra Rare')],
+  [i8ln('Common'), i8ln('Uncommon'), i8ln('Rare'), i8ln('Very Rare'), i8ln('Ultra Rare'), i8ln('New Spawn')]
 ]
 
 
@@ -2967,7 +2968,7 @@ $(function () {
         })
         $selectRarityNotify.select2({
             placeholder: i8ln('Select Rarity'),
-            data: [i8ln('Common'), i8ln('Uncommon'), i8ln('Rare'), i8ln('Very Rare'), i8ln('Ultra Rare')],
+            data: [i8ln('Common'), i8ln('Uncommon'), i8ln('Rare'), i8ln('Very Rare'), i8ln('Ultra Rare'), i8ln('New Spawn')],
             templateResult: formatState
         })
 
@@ -3033,9 +3034,10 @@ $(function () {
         $selectExcludeRarity.val(Store.get('excludedRarity')).trigger('change')
     })
 
-    // run interval timers to regularly update map and timediffs
+    // run interval timers to regularly update map, rarity and timediffs
     window.setInterval(updateLabelDiffTime, 1000)
     window.setInterval(updateMap, 5000)
+    window.setInterval(updatePokemonRarities, 300000)
     window.setInterval(updateGeoLocation, 1000)
 
     createUpdateWorker()
